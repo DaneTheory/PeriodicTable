71013c94fe7513e927c2e4b553e89759a0edab9c
hunter <hunterstich@gmail.com>
2014-07-25 16:55:46 -0500

    color by atomic weight and began working on individual element manipulation in more detail

--- src/views/PeriodicTable.js
+++ src/views/PeriodicTable.js
@@ -14,6 +14,9 @@ define(function(require, exports, module) {
     var Timer           = require('famous/utilities/Timer');
     var MouseSync       = require('famous/inputs/MouseSync');
     var Accumulator     = require('famous/inputs/Accumulator');
+    var EventHandler = require('famous/core/EventHandler');
+    var EventMapper   = require('famous/events/EventMapper');
+
 
 
     function PeriodicTable() {
@@ -37,6 +40,7 @@ define(function(require, exports, module) {
         var tableXDefault = 0;
         var xTransitionable = new Transitionable(tableXDefault);
 
+
         var defaultRotationSpeed = 0;
         var xRotationSpeed = new Transitionable(defaultRotationSpeed);
 
@@ -46,6 +50,8 @@ define(function(require, exports, module) {
         this.animationTransitionable = new Transitionable(false);
         this.constructed = true;
         this.currentElement = '';
+        this.isColored = false;
+
 
 
         /****************ACCUMULATOR VARIABLES****************/
@@ -67,15 +73,15 @@ define(function(require, exports, module) {
           //console.log(mousePosition[0]);
           yTransitionable.set(mousePosition[0] / 150);
           xTransitionable.set(mousePosition[1] / -150);
+          //console.log('x & y Rotation: ' + xTransitionable.get() + ', ' + yTransitionable.get())
+          var planeRotation = {xRotation: xTransitionable.get(), yRotation: yTransitionable.get()};
+          this._eventOutput.emit('planeChanged', planeRotation);
 
-          //console.log()
-        });
+          }.bind(this));
 
         mouseSync.on("end", function(data) {
           xRotationSpeed.set(data.velocity[0]);
-          //smallQuaternion.add(xRotationSpeed.get());
         })
-
 
 
         var quaternion = new Quaternion(1, 0, 0, 0);
@@ -106,7 +112,7 @@ define(function(require, exports, module) {
 
 
         var translateNode = mainEngine.add(this.translateModifier);
-        translateNode.add(rotationModifier).add(createTable(elementSize,elementSize,elementSize, tableWidth, tableHeight, this.options.elementData));
+        translateNode.add(rotationModifier).add(createTable(this, elementSize,elementSize,elementSize, tableWidth, tableHeight, this.options.elementData));
 
         if (this.runRandomAnimation) {
         Timer.setTimeout(function() {
@@ -123,6 +129,7 @@ define(function(require, exports, module) {
 
 
         _createViewButton.call(this);
+        _setPlanePositionListener.call(this);
 
         this.add(mainEngine);
 
@@ -151,31 +158,40 @@ define(function(require, exports, module) {
       startStopDepthAnimation(this.runRandomAnimation);
 
     }
+    PeriodicTable.prototype.colorWeight = function() {
+      for (var i = 0; i < 120; i ++) {
+        colorByWeight(i, this.options.elementData, this.isColored);
+      }
+      this.isColored = !this.isColored;
+    }
+
+
+    PeriodicTable.prototype.checkPosition = function() {
+      console.log(this.elementSize);
+    }
+
+
+
+
 
     PeriodicTable.DEFAULT_OPTIONS = {
       elementData: {},
-      buttonFontSize: '17px',
-      none: 'rgba(255, 255, 255, 0.54)',
-      hydrogen: 'rgba(102, 102, 102, 0.54)',
-      alkaliMetals: 'rgba(245, 176, 120, 0.54)',
-      alkaliEarthMetals: 'rgba(241, 245, 120, 0.54)',
-      transitionMetals: 'rgba(242, 118, 118, 0.54)',
-      poorMetals: 'rgba(120, 245, 220, 0.54)',
-      otherNonMetals: 'rgba(132, 245, 120, 0.54)',
-      nobleGases: 'rgba(120, 159, 245, 0.54)',
-      lanthanoids: 'rgba(245, 120, 220, 0.54)',
-      actinoids: 'rgba(195, 120, 245, 0.54)'
+      buttonFontSize: '17px'
     };
 
-    function createTable(width, height, depth, tableWidth, tableHeight, elementData) {
+    function createTable(content, width, height, depth, tableWidth, tableHeight, elementData) {
       var table = new RenderNode();
-
+      var context = content;
       var elementNumber = 0;
       this.elementSurfaces = [];
+      this.elementBackSurfaces = [];
       this.translateModifiers = [];
       this.planeModifiers = [];
       this.backModifiers = [];
       this.backPlaneModifiers = [];
+      this.individualModifiers = [];
+      this.individualBackModifiers = [];
+
 
 
         function createElement(params){
@@ -191,6 +207,8 @@ define(function(require, exports, module) {
             transform: params.transform
           });
 
+          var individualModifier = new Modifier({});
+
           var backSurface = new Surface({
             size:params.size,
             classes: params.classes,
@@ -201,20 +219,21 @@ define(function(require, exports, module) {
             transform: params.transformBack
           });
 
+          var individualBackModifier = new Modifier({});
+
 
           var planeModifier = new Modifier();
           var backPlaneModifier = new Modifier();
 
           var node = table.add(modifier);
-          node.add(planeModifier).add(surface);
+          var modifierNode = node.add(planeModifier)
+          modifierNode.add(individualModifier).add(surface);
 
           var nodeBack = table.add(backModifier);
-          nodeBack.add(backPlaneModifier).add(backSurface);
-
-
+          var modifierBackNode = nodeBack.add(backPlaneModifier);
+          modifierBackNode.add(individualBackModifier).add(backSurface);
 
           elementNumber++;
-
 
           surface.on('click', function() {
             var name = surface.getContent();
@@ -225,23 +244,21 @@ define(function(require, exports, module) {
             }
 
 
-            elementClicked({name: name, surface: surface, modifier: modifier, backSurface: backSurface, backModifier: backModifier, planeModifier: planeModifier, params: params.transform, backParams: params.transformBack});
+            var elementObject = {elementNumber: params.elementNumber, context: context, name: name, surface: surface, modifier: modifier, individualModifier: individualModifier, backSurface: backSurface, backModifier: backModifier, individualBackModifier: individualBackModifier, planeModifier: planeModifier, params: params.transform, backParams: params.transformBack};
+            elementClicked(elementObject);
+            context.checkPosition();
+            }.bind(this));
 
 
-
-
-
-            //console.log(this.currentElement);
-
-          }.bind(this));
-
-
-          this.elementSurfaces.push(surface);
+          this.elementSurfaces.push({surface: surface, properties: params.properties});
+          this.elementBackSurfaces.push({surface: backSurface, properties: params.properties});
           this.translateModifiers.push(
             {modifier: modifier, translate: params.transform});
           this.planeModifiers.push(planeModifier);
           this.backModifiers.push({modifier: backModifier, translate: params.transformBack});
           this.backPlaneModifiers.push(backPlaneModifier);
+          this.individualModifiers.push(individualModifier);
+          this.individualBackModifiers.push(individualBackModifier);
         };
 
 
@@ -292,7 +309,6 @@ define(function(require, exports, module) {
       'lanthanoids': 'rgba(245, 120, 220, 0.54)',
       'actinoids': 'rgba(195, 120, 245, 0.54)'
     };
-      console.log(elementColors);
 
       var elementDataset = elementData;
       var elementNumber = 0;
@@ -328,12 +344,12 @@ define(function(require, exports, module) {
               color: 'white'
             },
             transform: Transform.translate((- tableWidth / 2) + (distance * rowNumber), (- tableHeight / 2) + (distance * columnNumber), 0),
-            transformBack: Transform.multiply(Transform.translate((- tableWidth / 2) + (distance * rowNumber), (- tableHeight / 2) + (distance * columnNumber), 0), Transform.multiply(Transform.rotateZ(Math.PI), Transform.rotateX(Math.PI)))
+            transformBack: Transform.multiply(Transform.translate((- tableWidth / 2) + (distance * rowNumber), (- tableHeight / 2) + (distance * columnNumber), 0), Transform.multiply(Transform.rotateZ(Math.PI), Transform.rotateX(Math.PI))),
+            elementNumber: elementNumber
           });
         }
 
       }
-
 
       return table;
     }
@@ -445,8 +461,6 @@ define(function(require, exports, module) {
         curve: 'easeOut',
         duration: 1000
       });
-
-
       this.constructed = true;
 
     }
@@ -491,7 +505,6 @@ define(function(require, exports, module) {
 
       this.createCubeButton.on('click', function() {
         this.constructCube();
-        //_createCube(this.elementSize);
       }.bind(this));
 
       this.randomDepthAnimationButton = new Surface({
@@ -515,22 +528,112 @@ define(function(require, exports, module) {
         this.scatterTable();
       }.bind(this));
 
+      this.colorByWeightButton = new Surface({
+        size: [true, true],
+        content: 'COLOR WEIGHT',
+        properties: {
+          fontSize: this.options.buttonFontSize,
+          padding: '10px',
+          marginTop: '150px',
+          fontFamily: 'HelveticaNeue-Light',
+          fontWeight: '100'
+        }
+      });
 
+      this.colorByWeightButton.addClass('button');
+
+      this.add(this.colorByWeightButton);
+
+      this.colorByWeightButton.on('click', function() {
+        this.colorWeight();
+      }.bind(this));
 
     }
 
-    function elementClicked(name) {
-      //console.log(name);
+    function colorByWeight(elementNumber, elementData, isColored) {
+      var colored = isColored;
+      var elementDataset = elementData;
+      var atomicWeight = elementDataset[elementNumber].atomicWeight;
 
-      var elementName = name;
-      this.currentElement = name;
+      var calculatedR = Math.round(atomicWeight);
+      if (calculatedR > 255) {
+        calculatedR = 255;
+      }
+
+      var calculatedB = Math.round(atomicWeight);
+      if (calculatedB > 255) {
+        calculatedB = 255;
+      }
 
 
-      elementName.modifier.setTransform(Transform.multiply(Transform.translate(0,0,800), Transform.multiply(Transform.rotateZ(-Math.PI), Transform.rotateX(-Math.PI))), {
+      //console.log(this.elementBackSurfaces[elementNumber].surface.getProperties());
+
+      //math math Math
+      var weightPercent = atomicWeight / 2.94;
+      var rgbaPercent = weightPercent * 1.82;
+      var calculatedColor = Math.round((rgbaPercent - 182) * -1);
+      console.log(atomicWeight + '   ' + calculatedColor);
+
+      var weightedColor = 'rgba(' + 240 + ', ' + calculatedColor + ', ' + calculatedColor + ', 0.54)';
+
+      if (!colored) {
+      this.elementSurfaces[elementNumber].surface.setProperties({backgroundColor: weightedColor});
+      this.elementBackSurfaces[elementNumber].surface.setProperties({backgroundColor: weightedColor});
+      } else {
+      var resetColor = this.elementSurfaces[elementNumber].properties['backgroundColor']
+      //console.log('reset color: ' + resetColor);
+      this.elementSurfaces[elementNumber].surface.setProperties({backgroundColor: resetColor});
+      this.elementBackSurfaces[elementNumber].surface.setProperties({backgroundColor: resetColor});
+      }
+    }
+
+
+//////////////////////////////////////INDIVIDUAL ELEMENT INTERACTION///////////////////////////////////////////
+
+    function _setPlanePositionListener() {
+      var planeDefaults = [0,0];
+      this.planeRotation = new Transitionable(planeDefaults);
+
+      this.on('planeChanged', function(rotation) {
+        this.planeRotation.set([rotation.xRotation, rotation.yRotation]);
+        console.log(this.planeRotation.get());
+
+      });
+    }
+
+    function elementClicked(elementClicked) {
+
+
+      var element = elementClicked;
+      var elementNumber = element.elementNumber;
+      var currentPlaneRotation = element.context.planeRotation.get();
+      //var elementCurrentRotation = this.translateModifiers[elementNumber].modifier.getTransform();
+
+
+      console.log(element.context.planeRotation.get());
+      //console.log(elementCurrentRotation);
+
+      currentElement = elementClicked;
+
+      element.modifier.halt();
+      element.backModifier.halt();
+
+//BACK CUBE MOD: Transform.multiply(Transform.translate(((-cubeSize / 2) + (elementSize / 2) + (distance * backColumnNumber)),((-cubeSize / 2) + (elementSize / 2) + (distance * backRowNumber)),-cubeSize/2), Transform.multiply(Transform.rotateZ(Math.PI), Transform.rotateX(Math.PI))),
+
+      this.translateModifiers[elementNumber].modifier.setTransform(Transform.multiply(Transform.translate(0,0,300), Transform.multiply(Transform.rotateY(0), Transform.rotateX(0))), {
         duration: 1500,
         curve: 'easeOut'
       });
-      elementName.backModifier.setTransform(Transform.multiply(Transform.translate(0,0,800), Transform.multiply(Transform.rotateZ(0), Transform.rotateX(0))), {
+      this.planeModifiers[elementNumber].modifier.setTransform(Transform.translate(0,0,0), {
+        duration: 1500,
+        curve: 'easeOut'
+      });
+
+      element.individualModifier.setTransform(Transform.multiply(Transform.translate(0,0,300), Transform.multiply(Transform.rotateY(-currentPlaneRotation[1]), Transform.rotateX(-currentPlaneRotation[0]))), {
+        duration: 1500,
+        curve: 'easeOut'
+      });
+      element.individualBackModifier.setTransform(Transform.multiply(Transform.translate(0,0,-300), Transform.multiply(Transform.rotateY(-currentPlaneRotation[1]), Transform.rotateX(currentPlaneRotation[0]))), {
         duration: 1500,
         curve: 'easeOut'
       });
@@ -540,19 +643,20 @@ define(function(require, exports, module) {
     function elementReturn() {
       //console.log(this.currentElement);
 
-      this.currentElement.modifier.setTransform(
-        this.currentElement.params,
-        {
-        duration: 1500,
-        curve: 'easeOut'
-      });
-
-      this.currentElement.backModifier.setTransform(this.currentElement.backParams, {
-        duration: 1500,
-        curve: 'easeOut'
-      });
+      // this.currentElement.modifier.setTransform(
+      //   this.currentElement.params,
+      //   {
+      //   duration: 1500,
+      //   curve: 'easeOut'
+      // });
+      //
+      // this.currentElement.backModifier.setTransform(this.currentElement.backParams, {
+      //   duration: 1500,
+      //   curve: 'easeOut'
+      // });
 
     }
+
 
 //////////////////////////////////////CUBE CREATION///////////////////////////////////////////
     function _createCube(elementDimentions) {
@@ -890,24 +994,6 @@ define(function(require, exports, module) {
         });
 
       }
-//
-      // var backOpacity = new Transitionable(1);
-      // for (var i = 0; i < 120; i++){
-      //
-      //   this.backPlaneModifiers[i].opacityFrom(function() {
-      //     return backOpacity.get();
-      //   });
-      //
-      //   backOpacity.set(0, {
-      //     method: 'tween',
-      //     curve: 'easeOut',
-      //     duration: 1000
-      //   });
-      // }
-
-
-
-
 
     }
 
